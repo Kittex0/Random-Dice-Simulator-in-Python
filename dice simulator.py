18a3fb06f3062831023a9383a515bf09adef56f3
Kittex0 <83418310+Kittex0@users.noreply.github.com>
2021-07-10 13:49:05 +0300

    Added Comments

--- dice simulator.py
+++ dice simulator.py
@@ -1,13 +1,17 @@
+# These are the modules needed, the time module is customary while the random module in mandotory
 import time
 import random
 
+# Intro
 print("Dice Simulator")
 time.sleep(2)
 
 
 i = True
 
+
 while i == True:
+    # Chooses the random number from 1-6 and then displays it.
     input("Press enter to roll the dice: ")
     dice_choice = random.choice(range(1, 7))
     print(f"It was: {dice_choice}")
